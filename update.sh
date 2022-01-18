59b708f33cc57b18e71a8a9cb457bb9b379d53fa
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-01-18 12:13:49 +0800

    Update update.sh
    * put zip files into zipped/ instead
    * Update submodules instead of git pull
    * Add skip message
    * Store line separator in a variable

--- update.sh
+++ update.sh
@@ -6,6 +6,11 @@ function print_info() {
   echo -e "\e[1;32m[*]\e[0m $1"
 }
 
+# pretty warning print function
+function print_warning() {
+  echo -e "\e[1;33m[*]\e[0m $1"
+}
+
 # pretty error print function
 function print_error() {
   echo -e "\e[1;31m[*]\e[0m $1"
@@ -26,11 +31,22 @@ function handle_cd_error() {
 # query list of folders in the current directory
 folders=$(ls -d */ | sed 's/\///g')
 ignore_folders=$(sed 's/#.*//g' .gitignore | sed 's/\///g')
-
+line="----------------------------------------------------"
+extra_ignore_folders=(
+  "zipped"
+)
 enable_compression=true
 
 ##########################################################
 ##########################################################
+
+case $1 in
+  -c | --clean | --fresh)
+    rm -rf zipped/*
+    ;;
+  *)
+    # do nothing
+esac
 
 print_info "Note: all output will be redirected to update.log"
 
@@ -40,30 +56,35 @@ if [ -f update.log.lz4 ]; then
   lz4 --rm -qd update.log.lz4
 fi
 
+# Update submodules
+git submodule update
+
 for folder in $folders; do
 
   # skip folders in .gitignore
   if [[ $ignore_folders =~ $folder ]]; then
+    print_info "$line"
+    print_warning "Skipping folder: $folder"
     continue
   fi
 
-  print_info "----------------------------------------------------"
+  # skip extra ignore folders
+  for extra_ignore_folder in "${extra_ignore_folders[@]}"; do
+    if [[ $folder =~ $extra_ignore_folder ]]; then
+      print_info "$line"
+      print_warning "Skipping folder: $folder"
+      continue 2
+    fi
+  done
 
-  # run git pull in each folder
-  print_info "Updating $folder"
-  (
-    cd "$folder" || exit 5
-    git clean -df
-    git stash
-    git pull
-  ) &>> update.log
-  handle_cd_error "$folder"
+  print_info "$line"
 
   # enter every folder and zip the content recursively
   print_info "Zipping $folder"
+  mkdir -p zipped
   (
     cd "$folder" || exit 5
-    zip -r "$folder.zip" *
+    zip -r "../zipped/$folder.zip" *
   ) &>> update.log
   handle_cd_error "$folder"
 
@@ -78,7 +99,7 @@ for folder in $folders; do
 done
 
 if [[ $enable_compression == true ]]; then
-  print_info "----------------------------------------------------"
+  print_info "$line"
   print_info "Compressing log file"
   lz4 --rm -q update.log
 fi
